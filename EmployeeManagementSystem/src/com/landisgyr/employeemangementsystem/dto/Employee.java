752ecb2ee3eadd0fd961e6ea3e26ca7719a0faa7
abhinandan chivate <abhinandanchivate2000@gmail.com>
2021-07-02 16:44:15 +0530

    inheritance update

--- EmployeeManagementSystem/src/com/landisgyr/employeemangementsystem/dto/Employee.java
+++ EmployeeManagementSystem/src/com/landisgyr/employeemangementsystem/dto/Employee.java
@@ -8,7 +8,7 @@ public class Employee {
 	private String empId;
 	private String empFirstName;
 	private String empLastName;
-	private float empSalary;
+	protected float empSalary;
 	private String permanantAddress;
 	private String currentAddress;
 	private String contactNumber;
@@ -16,18 +16,34 @@ public class Employee {
 	// parameterized constructor.// can we provide the values
 	public Employee(String empId, String empFirstName, String empLastName, float empSalary, String permanantAddress,
 			String contactNumber) {
-		super();
-		this.empId = empId;
-		this.empFirstName = empFirstName;
-		this.empLastName = empLastName;
-		this.empSalary = empSalary;
+		this(empId, empFirstName, empLastName, empSalary);
 		this.permanantAddress = permanantAddress;
 		this.contactNumber = contactNumber;
 	}
 	public Employee() {
 		// TODO Auto-generated constructor stub
+		System.out.println("hello from Employee");
+	}
+	public Employee(String empId, String empFirstName, String empLastName, float empSalary, String permanantAddress,
+			String currentAddress, String contactNumber, String location) {
+//		super();
+	this(empId, empFirstName, empLastName, empSalary);
+		this.permanantAddress = permanantAddress;
+		this.currentAddress = currentAddress;
+		this.contactNumber = contactNumber;
+		this.location = location;
+	}
+	public Employee(String empId, String empFirstName, String empLastName, float empSalary) {
+		super();
+		this.empId = empId;
+		this.empFirstName = empFirstName;
+		this.empLastName = empLastName;
+		this.empSalary = empSalary;
 	}
 
+	public float calculateSalary() {
+		return this.empSalary + 200;
+	}
 
 	
 }
